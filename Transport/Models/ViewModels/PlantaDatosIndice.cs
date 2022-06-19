6a6ab8323890f63b1a0fec4187ba6328a091f1db
98Rubent <torrestenrique98@gmail.com>
2022-06-18 23:22:55 -0600

    Publicacion ViewModels

---
+++ Transport/Models/ViewModels/PlantaDatosIndice.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Transport.Models.Tablas;
+
+namespace Transport.Models.ViewModels
+{
+    public class PlantaDatosIndice
+    {
+        public IEnumerable<Planta> Plantas { get; set; }
+        public IEnumerable<ProductoAsignado> ProductosAsignados { get; set; }
+        public IEnumerable<Producto> Productos { get; set; }
+    }
+}
---
+++ Transport/Models/ViewModels/ProductosAsignadosData.cs
@@ -0,0 +1,9 @@
+﻿namespace Transport.Models.ViewModels
+{
+    public class ProductosAsignadosData
+    {
+        public int ProductoID { get; set; }
+        public string Nombre { get; set; }
+        public bool Asignado { get; set; }
+    }
+}
